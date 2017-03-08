4cc1fe3debfb4e0fce4bbaff617edec994cf750c
ashish gahlot <dreamashish21@gmail.com>
2017-03-08 17:12:43 +0530

    GDT structure error
    As explained by alexfru, the struct is reversed.

--- io/gdt.c
+++ io/gdt.c
@@ -1,16 +1,15 @@
 #include <boot.h>
 #include <x86.h>
-#include <stdint.h>
 
 tss_entry_t tss_entry;
 
 struct gdt_entry {
-		unsigned char base_high;
-		unsigned char granularity;
-		unsigned char access;
-		unsigned char base_middle;
-		unsigned short base_low;
 		unsigned short limit_low;
+		unsigned short base_low;
+		unsigned char base_middle;
+		unsigned char access;
+		unsigned char granularity;
+		unsigned char base_high;
 } __attribute__((packed));
 
 /* GDT pointer which points to our gdt structure */
@@ -19,10 +18,10 @@ struct gdt_ptr {
 	unsigned int base;
 } __attribute__((packed));
 
-struct gdt_entry gp[3];
+struct gdt_entry gp[5];
 struct gdt_ptr gdpt;
 
-void set_gdt(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned granularity)
+void set_gdt(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran)
 {
 	gp[num].base_low = (base & 0xFFFF);
 	gp[num].base_middle = (base >> 16) & 0xFF;
@@ -34,10 +33,10 @@ void set_gdt(int num, unsigned long base, unsigned long limit, unsigned char acc
 
 	/* granularity bit is from 16-24 */
 	gp[num].granularity = (limit >> 16) & 0x0F;
-	gp[num].granularity |= (granularity & 0xF0);
+	gp[num].granularity |= (gran & 0xF0);
 }
 
-static void set_tss(int num, u16 ss0, u32 esp0)
+void set_tss(unsigned int num, unsigned short ss0, unsigned int esp0)
 {
 	uintptr_t base;
 	unsigned long limit;
@@ -47,7 +46,7 @@ static void set_tss(int num, u16 ss0, u32 esp0)
 	limit = base + sizeof(tss_entry);
 
 	/* Add tss descriptor to gdt */
-	set_gdt(3, base, limit, 0xE9, 0xCF);
+	set_gdt(num, base, limit, 0xE9, 0x00);
 
 	memset(&tss_entry, 0, sizeof(tss_entry));
 
@@ -55,30 +54,32 @@ static void set_tss(int num, u16 ss0, u32 esp0)
 	tss_entry.esp0 = esp0;
 
 	tss_entry.cs = 0x0b;
-	tss_entry.ss = 
-	tss_entry.ds = 
-	tss_entry.es = 
-	tss_entry.fs = 
+	tss_entry.ss = 0x13;
+	tss_entry.ds = 0x13;
+	tss_entry.es = 0x13;
+	tss_entry.fs = 0x13;
 	tss_entry.gs = 0x13;
 	tss_entry.iomap_base = sizeof(tss_entry);
 }
 
 void gdt(void)
 {
-	gdpt.limit = (sizeof(struct gdt_entry)*3)-1;
-	gdpt.base = (unsigned int)&gp;
+	gdpt.limit = (sizeof(struct gdt_entry)*5)-1;
+	gdpt.base = (uintptr_t)&gp;
 
 	set_gdt(0, 0, 0, 0, 0);
-	set_gdt(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
-	set_gdt(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
+	set_gdt(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); /* kernel code segment */
+	set_gdt(2, 0, 0xFFFFFFFF, 0x92, 0xCF); /* kernel data segment */
+	set_gdt(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); /* User mode code segment */
+    set_gdt(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); /* User mode data segment */
 
-	set_tss(3, 0x10, 0);
+	set_tss(5, 0x10, 0);
 
-	gdt_flush();
+	gdt_flush((uintptr_t)&gdpt);
 	tss_flush();
 }
 
-void set_kernel_stack(unsigned long stack)
+void set_kernel_stack(uintptr_t stack)
 {
 	tss_entry.esp0 = stack;
 }
